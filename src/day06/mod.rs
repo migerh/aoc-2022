432e42a61fb10c83d11d8988966b5e4a494f219c
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2022-12-06 06:18:56 +0100

    Day 6: Add solution

---
+++ src/day06/mod.rs
@@ -0,0 +1,59 @@
+use std::{num::ParseIntError, collections::HashSet};
+use crate::utils::ParseError;
+
+#[aoc_generator(day06)]
+pub fn input_generator(input: &str) -> Result<Vec<char>, ParseIntError> {
+    Ok(input.chars().collect::<Vec<_>>())
+}
+
+fn solve(input: &Vec<char>, length: usize) -> usize {
+    let mut result = length;
+
+    for w in input.windows(length) {
+        let h: HashSet<char> = HashSet::from_iter(w.iter().cloned());
+
+        if h.len() == length {
+            break;
+        }
+
+        result += 1;
+    }
+
+    result
+}
+
+#[aoc(day06, part1)]
+pub fn solve_part1(input: &Vec<char>) -> Result<usize, ParseError> {
+    Ok(solve(input, 4))
+}
+
+#[aoc(day06, part2)]
+pub fn solve_part2(input: &Vec<char>) -> Result<usize, ParseError> {
+    Ok(solve(input, 14))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::utils::ParseError;
+
+    fn sample() -> &'static str {
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    }
+
+    fn input() -> Result<Vec<char>, ParseError> {
+        Ok(input_generator(sample())?)
+    }
+
+    #[test]
+    fn part1_sample() -> Result<(), ParseError> {
+        let data = input()?;
+        Ok(assert_eq!(10, solve_part1(&data)?))
+    }
+
+    #[test]
+    fn part2_sample() -> Result<(), ParseError> {
+        let data = input()?;
+        Ok(assert_eq!(29, solve_part2(&data)?))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 pub mod utils;
 
 aoc_lib!{ year = 2022 }
