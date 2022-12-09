fdb25cb3636727ad9457270f67ec7ad90c3334cb
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2022-12-09 22:58:10 +0100

    Update template for new error handling

--- src/template/mod.rs
+++ src/template/mod.rs
@@ -1,46 +1,47 @@
 use std::num::ParseIntError;
-use crate::utils::ParseError;
+
+use anyhow::{Result, Context};
 
 #[aoc_generator(dayXX)]
-pub fn input_generator(input: &str) -> Result<Vec<i32>, ParseIntError> {
+pub fn input_generator(input: &str) -> Result<Vec<i32>> {
     input
         .lines()
-        .filter(|s| *s != "")
+        .filter(|s| !s.is_empty())
         .map(|s| s.parse::<i32>())
         .collect::<Result<Vec<_>, ParseIntError>>()
+        .context("Error while parsing input")
 }
 
 #[aoc(dayXX, part1)]
-pub fn solve_part1(input: &Vec<i32>) -> Result<i32, ParseError> {
-    Ok(0)
+pub fn solve_part1(input: &[i32]) -> Result<usize> {
+    Ok(input.len())
 }
 
 #[aoc(dayXX, part2)]
-pub fn solve_part2(input: &Vec<i32>) -> Result<i32, ParseError> {
-    Ok(0)
+pub fn solve_part2(input: &[i32]) -> Result<usize> {
+    Ok(input.len())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::utils::ParseError;
 
     fn sample() -> &'static str {
         ""
     }
 
-    fn input() -> Result<Vec<i32>, ParseError> {
-        Ok(input_generator(sample())?)
+    fn input() -> Result<Vec<i32>> {
+       input_generator(sample())
     }
 
     #[test]
-    fn part1_sample() -> Result<(), ParseError> {
+    fn part1_sample() -> Result<()> {
         let data = input()?;
         Ok(assert_eq!(0, solve_part1(&data)?))
     }
 
     #[test]
-    fn part2_sample() -> Result<(), ParseError> {
+    fn part2_sample() -> Result<()> {
         let data = input()?;
         Ok(assert_eq!(0, solve_part2(&data)?))
     }
