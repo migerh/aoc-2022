573f4173166d7fe4245b2e39994800fdeebbc3a4
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2022-12-21 21:16:27 +0100

    Day 16: Part I

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,20 +1,17 @@
-use std::collections::HashSet;
-use std::str::FromStr;
+use std::{collections::{HashMap, HashSet}, str::FromStr, cmp::max};
 
 use anyhow::{Context, Error, Result};
-use pathfinding::prelude::dijkstra_all;
 use pathfinding::prelude::dijkstra;
 use regex::Regex;
+
+type DistanceMap<'a> = HashMap<&'a Valve, Vec<(&'a Valve, usize)>>;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Valve {
     name: String,
-    index: usize,
     flow_rate: usize,
     valves: Vec<String>,
 }
-
-type Node = (Valve, usize, usize, isize, Vec<bool>);
 
 impl FromStr for Valve {
     type Err = Error;
@@ -47,7 +44,6 @@ impl FromStr for Valve {
 
         Ok(Valve {
             name,
-            index: 0,
             flow_rate,
             valves,
         })
@@ -64,81 +60,186 @@ pub fn input_generator(input: &str) -> Result<Vec<Valve>> {
         .context("Error while parsing input")
 }
 
-fn get_neighbors(valves: &[Valve], pos: &Node) -> Vec<(Node, isize)> {
-    if pos.1 >= 30 {
-        return vec![];
+fn resolve_valve<'a>(valves: &'a [Valve], name: &String) -> Option<&'a Valve> {
+    valves.iter().find(|v| &v.name == name)
+}
+
+fn get_neighbors<'a>(valves: &'a [Valve], valve: &Valve) -> Vec<(&'a Valve, usize)> {
+    valve
+        .valves
+        .iter()
+        .filter_map(|v| Some((resolve_valve(valves, v)?, 1)))
+        .collect::<Vec<_>>()
+}
+
+fn pre_calc_distances(valves: &[Valve]) -> DistanceMap {
+    let names = valves.iter().map(|v| v.name.to_owned()).collect::<Vec<_>>();
+    let mut map = HashMap::new();
+
+    for start in names.iter() {
+        let mut distances = vec![];
+        let start_valve = if let Some(node) = resolve_valve(valves, start) {
+            node
+        } else {
+            continue;
+        };
+
+        for end in names.iter().filter(|&v| v != start) {
+            let end_valve = if let Some(node) = resolve_valve(valves, end) {
+                node
+            } else {
+                continue;
+            };
+
+            if let Some(distance) = dijkstra(
+                &start_valve,
+                |valve| get_neighbors(valves, valve),
+                |valve| &valve.name == end,
+            ) {
+                distances.push((end_valve, distance.1));
+            }
+        }
+
+        distances.sort_by(|a, b| b.0.flow_rate.cmp(&a.0.flow_rate));
+        let distances = distances.into_iter().filter(|&(v, _)| v.flow_rate > 0).collect::<Vec<_>>();
+        map.entry(start_valve).or_insert(distances);
     }
 
-    let all_neighbor_valves = valves
+    map
+}
+
+#[derive(Debug, Clone)]
+pub struct State<'a> {
+    opened: HashSet<&'a Valve>,
+    visited: HashSet<&'a Valve>,
+    time: usize,
+    location: &'a Valve,
+    total_flow: usize,
+}
+
+impl<'a> State<'a> {
+    fn new(start: &'a Valve) -> Self {
+        State {
+            opened: HashSet::new(),
+            visited: HashSet::new(),
+            time: 0,
+            location: start,
+            total_flow: 0,
+        }
+    }
+
+    fn flow_during_time(&self, time: usize) -> usize {
+        self.opened.iter().map(|v| v.flow_rate).sum::<usize>() * time
+    }
+
+    fn next(&self, new_location: &'a Valve, distance: usize, open: bool, limit: usize) -> Option<Self> {
+        if self.opened.contains(&new_location) && open {
+            return None;
+        }
+
+        let time_consumed = distance + usize::from(open);
+        if self.time + time_consumed > limit {
+            return None;
+        }
+
+        let flow = self.flow_during_time(time_consumed);
+
+        let new_opened_valves = if open {
+            let mut new_opened = self.opened.clone();
+            new_opened.insert(new_location);
+            new_opened
+        } else {
+            self.opened.clone()
+        };
+
+        let mut new_visited = self.visited.clone();
+        new_visited.insert(new_location);
+
+        Some(State::<'a> {
+            opened: new_opened_valves,
+            visited: new_visited,
+            time: self.time + time_consumed,
+            location: new_location,
+            total_flow: self.total_flow + flow,
+        })
+    }
+
+    fn wait(&self, time: usize) -> Self {
+        let mut result = self.clone();
+        result.time += time;
+        result.total_flow += self.flow_during_time(time);
+
+        result
+    }
+
+    fn follow_up_states(&self, map: &'a DistanceMap, limit: usize) -> Vec<Self> {
+        let next = if let Some(n) = map.get(self.location) {
+            n
+        } else {
+            return vec![]
+        };
+
+        if self.opened.len() > next.len() {
+            return vec![self.wait(limit - self.time)];
+        }
+
+        let opened_iter = next
+            .iter()
+            // .filter(|&(v, _)| v.flow_rate > 0)
+            .filter(|&(v, _)| !self.opened.contains(v))
+            .filter(|&(v, _)| !self.visited.contains(v))
+            .filter_map(|&(v, d)| self.next(v, d, true, limit));
+
+        // let not_opened_iter = next
+        //     .iter()
+        //     // .filter(|&(v, _)| v.flow_rate > 0)
+        //     .filter(|&(v, _)| !self.opened.contains(v))
+        //     .filter(|&(v, _)| !self.visited.contains(v))
+        //     .filter_map(|&(v, d)| self.next(v, d, false));
+
+        // let mut result = opened_iter.chain(not_opened_iter).collect::<Vec<_>>();
+        let mut result = opened_iter.collect::<Vec<_>>();
+        result.push(self.wait(1));
+        result
+    }
+}
+
+fn total_flow(valves: &[Valve], map: &DistanceMap, limit: usize) -> Option<usize> {
+    let start = valves
         .iter()
-        .filter(|v| pos.0.valves.contains(&v.name));
-    
-    let unopened = all_neighbor_valves
-        .map(|v| {
-            ((v.clone(), pos.1 + 1, pos.2, pos.3, pos.4.clone()), 0)
-        });
+        .find(|v| &v.name == "AA")?;
 
+    let state = State::new(start);
+    let mut queue = vec![state];
+    let mut max_flow = 0;
 
-    let opened = valves
-        .iter()
-        .filter(|v| v.flow_rate > 0)
-        .filter(|v| pos.0.valves.contains(&v.name) && !pos.4[v.index])
-        .map(|v| {
-            let mut opened = pos.4.clone();
-            opened[v.index] = true;
+    let largest_flow = valves.iter().map(|v| v.flow_rate).sum::<usize>();
 
-            (
-                (
-                    v.clone(),
-                    pos.1 + 2,
-                    pos.2 + v.flow_rate,
-                    pos.3 - (v.flow_rate * (30 - pos.1 - 2)) as isize,
-                    opened,
-                ),
-                -((v.flow_rate * (30 - pos.1 - 2)) as isize),
-            )
-        });
+    while let Some(q) = queue.pop() {
+        max_flow = max(max_flow, q.total_flow);
 
-    opened.chain(unopened).collect::<Vec<_>>()
+        if q.time >= limit {
+            continue;
+        }
+
+        let best_case_remaining_flow = q.total_flow + (limit - q.time) * largest_flow;
+        if best_case_remaining_flow < max_flow {
+            continue;
+        }
+
+        queue.append(&mut q.follow_up_states(map, limit));
+    }
+
+    Some(max_flow)
 }
 
 #[aoc(day16, part1)]
-pub fn solve_part1(input: &[Valve]) -> Result<isize> {
-    let valves = input.iter().cloned().enumerate().map(|(i, mut v)| {
-        v.index = i;
-        v
-    }).collect::<Vec<_>>();
+pub fn solve_part1(input: &[Valve]) -> Result<usize> {
+    let limit = 30;
+    let map = pre_calc_distances(input);
+    let result = total_flow(input, &map, limit).context("Could not calculate")?;
 
-    let start =valves 
-        .iter()
-        .find(|v| v.name == "AA")
-        .context("Cannot find start")?;
-
-    let result = dijkstra_all(
-        &(start.clone(), 0, 0, 0, vec![false; valves.len()]),
-        |v| get_neighbors(&valves, v),
-        // |v| v.1 == 30,
-    ); // .context("Dijkstra failed")?;
-    println!("total results {}", result.len());
-
-    let max_flow = result.iter()
-        .map(|v| v.0)
-        .filter(|v| v.1 == 30)
-        .map(|v| v.3)
-        .min()
-        .context("Could not determine result")?;
-
-    // for r in result.iter().filter(|v| v.0.1 == 30).map(|v| v.0) {
-    //     println!("{:?}", r);
-    // }
-
-    println!("max {:?}", max_flow);
-
-    // for r in result {
-    //     println!("{:?}", r);
-    // }
-    // let total_flow = -result.1;
-    Ok(-max_flow)
+    Ok(result)
 }
 
 #[aoc(day16, part2)]
